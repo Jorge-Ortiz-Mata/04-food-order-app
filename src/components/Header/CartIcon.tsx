6384a40d4148c3c120b8ca477b96ff88a1ac1c09
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-03-29 00:43:55 -0600

    Adding Font-awesome.

--- src/components/Header/CartIcon.tsx
+++ src/components/Header/CartIcon.tsx
@@ -1,7 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faCoffee, faCartPlus } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee, faCartPlus);
+
 const CartIcon = () => {
 
   return (
-    <p>Icon</p>
+    <FontAwesomeIcon icon={["fas", "cart-plus"]} className="text-white text-sm" />
   )
 }
 
